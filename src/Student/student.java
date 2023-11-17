fef542a01d619dbc664207439b82dddaa62a2f98
Mrgumball2004 <arjie1419@gmail.com>
2023-11-17 17:03:57 +0800

    Added Student package

---
+++ src/Student/student.java
@@ -0,0 +1,20 @@
+package Student;
+
+public class student {
+    
+    
+    public static void main(String[] args) {
+       arjie put = new arjie();
+       
+       System.out.println("Enter your Name: ");
+       put.setName();
+       System.out.println("your Section: ");
+       put.setSection();
+       System.out.println("your Motto: ");
+       put.setMotto();
+       System.out.println("\nSTUDENT'S PROFILE!\n");
+       put.Info();
+        
+    }
+    
+}    
